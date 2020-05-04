2360bf4f094a86626b1a7c8001e8b2cfd601a61d
Gokhan Arli <gokhan@sylow.net>
2020-05-04 14:22:11 +0700

    Add cors to allow localhost to post to api

--- config/application.rb
+++ config/application.rb
@@ -33,11 +33,19 @@ module BridgeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.trailblazer.use_loader = true
+    #config.trailblazer.use_loader = true
 
     config.generators do |g|
       g.test_framework  false
       g.stylesheets     false
+    end    
+
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
     end    
   end
 end
